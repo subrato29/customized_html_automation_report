befa418db91f8265ff97e7b8ddafb33d000c598f
Subrato Sarkar <subratosarkar29@gmail.com>
2020-11-16 23:38:49 -0500

    Update SuiteReportUtil.groovy

--- SuiteReportUtil.groovy
+++ SuiteReportUtil.groovy
@@ -1,18 +1,5 @@
 package com.custom.report
 
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import com.kms.katalon.core.annotation.Keyword
-import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testobject.TestObject
-import internal.GlobalVariable
-
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
